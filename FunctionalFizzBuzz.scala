f680f46217e3743eefc17d3d9a2d7676d6e39f12
mopierotti <mopierotti@gmail.com>
2015-05-12 00:06:16 -0700

    Tabs -> Spaces

--- FunctionalFizzBuzz.scala
+++ FunctionalFizzBuzz.scala
@@ -1,23 +1,23 @@
 
 //Functional FizzBuzz Solution
 Range(1,101).map{ 
-	case n if n%3 == 0 && n%5 == 0 => s"FizzBuzz"
-	case n if n%3 == 0 => s"Fizz"
-	case n if n%5 == 0 => s"Buzz"
-	case n => s"$n"
+  case n if n%3 == 0 && n%5 == 0 => s"FizzBuzz"
+  case n if n%3 == 0 => s"Fizz"
+  case n if n%5 == 0 => s"Buzz"
+  case n => s"$n"
 }.mkString("\n")
 
 //Excessively Generic Functional FizzBuzz Solution
 def applyTestsToSequence[A,B](tests:Seq[(A => Option[B])], items:Seq[A]) = {
-	items.map { item =>
-		val appliedTests = tests.flatMap(_(item))
-		if (appliedTests.size >= 1) appliedTests.mkString("") else item.toString
-	}.mkString("\n")
+  items.map { item =>
+    val appliedTests = tests.flatMap(_(item))
+    if (appliedTests.size >= 1) appliedTests.mkString("") else item.toString
+  }.mkString("\n")
 }
 
 val tests:Seq[(Int => Option[String])] = Seq( //Order defined is order applied
-	((n:Int) => if (n % 3 == 0) Some("Fizz") else None),
-	((n:Int) => if (n % 5 == 0) Some("Buzz") else None)
+  ((n:Int) => if (n % 3 == 0) Some("Fizz") else None),
+  ((n:Int) => if (n % 5 == 0) Some("Buzz") else None)
 )
 
 print(applyTestsToSequence(tests, Range(1,101)))
