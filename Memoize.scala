b0af632e7df8039df1a73b19515e7cd048ed03fd
mopierotti <mopierotti@gmail.com>
2015-05-12 00:41:58 -0700

    Add comments to memoize

--- Memoize.scala
+++ Memoize.scala
@@ -1,3 +1,12 @@
+/*Simple memoize implementation
+    Caveats:
+        - Supports single-argument functions only
+        - Does not support concurrent threads (For that, switch to a concurrent mutable map)
+        - Does not support recursive functions directly
+    Notes:
+        - "If a tree falls in the woods, does it make a sound? If a pure function mutates local data to return an immutable value, is that ok?" 
+        Yes, yes it is.
+*/
 def memoize[A,B](func:(A => B)):(A => B) = {
   val mutableMap = scala.collection.mutable.Map.empty[A,B]
     (input:A) => {
